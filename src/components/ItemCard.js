fc664402db00050d1c80b218914c6fe409d6e436
Yuxiang Li <seanliyuxiang@gmail.com>
2021-10-28 09:46:58 -0500

    Got username and location to show on card

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function ItemCard({ item, targetUser, handleDeletion }) {
+function ItemCard({ item, targetUser, handleDeletion, owner }) {
     return (
         <div className="card">
             <img className="cardImage" src={item.image_url} alt={item.name}/>
             <div className="cardText">
                 <p>{item.name}</p>
-                <p>posted by: {item.owner_id} in -location here-</p>
+                <p>posted by: {owner.username} in -{owner.location}-</p>
                 {/* <h5>Username: {targetUser.username}</h5> */}
             </div>
             <button className="delete" onClick={() => handleDeletion(item)}>Delete item</button>
